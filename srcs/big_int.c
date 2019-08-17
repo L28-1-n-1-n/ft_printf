8f75f636b89152c22df64f6c0e6f2317ead5ce64
Henrietta LO <hlo@e1r8p21.42.fr>
2019-08-17 10:52:07 +0200

    BCD tested working, next step is to make it recursive and test recursion

--- srcs/big_int.c
+++ srcs/big_int.c
@@ -35,6 +35,36 @@ void  print_bits(uint64_t data, char *message, unsigned int number)
   printf("%s %s %d\n\n", str, message, number);
 }
 
+void full_row(uint64_t *raw, unsigned int row_no)
+{
+  uint64_t block;
+  unsigned int digit;
+  uint64_t mask;
+
+  block = 0;
+  mask = 0xF; // start at 9th bit
+  digit = 0;
+  printf("----------------------------     18    ---------------------------------\n");
+  print_bits(raw[row_no], "(init)", row_no);
+  while (block < 16) // 64/ 4 = 16, #0 - #15 = total 16 blocks
+  {
+//    printf("mask is %llu at block %d\n", mask, block);
+    digit = (raw[row_no] & mask) >> (block * 4);
+//    printf("raw[19] has value %llu\n", raw[19]);
+//    printf("mask is %llu\n", mask);
+//    printf("digit is %d\n", digit);
+    if (digit > 4)
+    {
+      raw[row_no] += ((uint64_t)3 << (block * 4));
+      print_bits(raw[row_no], "(add 3)", block);
+    }
+    //full_row(raw, row_no-1);
+    mask = (block == 15) ? 0xF : mask << 4;
+    block++;
+  }
+  //eventually we need to copy the last bit of this row and call full_row on row_no - 1 , etc.
+}
+
 void  within_row(uint64_t *raw, unsigned int shift)
 {
   uint64_t block;
@@ -47,9 +77,19 @@ void  within_row(uint64_t *raw, unsigned int shift)
   digit = 0;
   while (shift) // loop until copied number is shifted 8 times
   {
-    raw[19] = raw[19] << 1;
+    if (raw[19] & 0x8000000000000000)
+    {
+      if (raw[18])
+        raw[18] <<= 1;
+      raw[18] |= 1; // and afterwards if raw[18] is not zero , each time we shift raw[19] we will have to shift raw[18] too
+    }
+    else
+      if (raw[18]) // case bit 64 of raw[19] is zero, but raw [18] still needs to be shifted along with next shift
+        raw[18] <<= 1;
+    raw[19] <<= 1;
     shift_count++;
     block = 0;
+    printf("-------------------------------------------     19    ---------------------------------\n");
     print_bits(raw[19], "(shift)", shift);
     while (block < 14) // (64 - 8) / 4 = 14, #0 - #13 = total 14 blocks
     {
@@ -62,14 +102,15 @@ void  within_row(uint64_t *raw, unsigned int shift)
       {
         raw[19] += ((uint64_t)3 << (8 + block * 4));
         print_bits(raw[19], "(add 3)", block);
-        if (block == 6)
-          printf("%llu is added to the above value\n", ((uint64_t)3 << (8 + block * 4)));
       }
-      mask = (block == 13) ? 0xF00 : mask << 4;
+      mask = (block == 13) ? 0xF00 : mask << 4; //reset mask to block 1 before exit loop at block == 14
       block++;
     }
+    if(raw[18])
+      full_row(raw, 18);
     shift--;
   }
+  printf("-------------------------------------------     19    ---------------------------------\n");
   print_bits(raw[19], "RESULT", 0);
   printf("\n\nShift_count is %d\n", shift_count);
 }
@@ -87,7 +128,7 @@ void  big_int(t_float fnum)
   raw[19] |= (((fnum.mantissa & 0xFE00000000000) >> z) + 0x80); // add most significant 7 bits of mantissa to raw , then put 1 in froht of most sig. bit
   while (z >= 5)
   {
-    print_bits(raw[19], "(init)", 0);
+    print_bits(raw[19], "(init)", 19);
     within_row(raw, 8);
     man_mask >>= 8;
     z -= 8;
@@ -96,16 +137,32 @@ void  big_int(t_float fnum)
     else
       raw[19] |= ((fnum.mantissa & man_mask) << (-1 * z));
   }
-  print_bits(raw[19], "(init)", 0);
+  print_bits(raw[19], "(init)", 19);
   within_row(raw, 8);
   remain_z = fnum.exponent - 52 + z;
-  print_bits(raw[19], "(init)", 0);
+  print_bits(raw[19], "(init)", 19);
   while (remain_z > 8)
   {
     within_row(raw, 8);
     remain_z -= 8;
   }
-  within_row(raw, remain_z);
+  within_row(raw, remain_z - 1);
+  printf("-------------------------------------------     ONE LAST SHIFT    ---------------------------------\n");
+  if (raw[19] & 0x8000000000000000)
+  {
+    if (raw[18])
+      raw[18] <<= 1;
+    raw[18] |= 1; // and afterwards if raw[18] is not zero , each time we shift raw[19] we will have to shift raw[18] too
+  }
+  else
+    if (raw[18]) // case bit 64 of raw[19] is zero, but raw [18] still needs to be shifted along with next shift
+      raw[18] <<= 1;
+  raw[19] <<= 1;
+  print_bits(raw[18], "RESULT", 18);
+  print_bits(raw[19], "RESULT", 19);
+  printf("-------------------     When you read the result, make sure to ignore the last 00000000 of raw[19]   ---------------\n");
+  printf("--------------TO DO : write recursive function so that row[18] calls row[17] etc etc    ----------------------------\n");
+  printf("--------------TO DO : write function to translate the result number into digits and then result string    ----------\n");
   // next step is to copy the last 5 bits from mantissa, then << 3, then copy that to raw[19] and shift one cycle
   // then repeat cycle calling on raw[19] alone, until exponent - 52 nos. of zeroes has been shifted
   // for that, we need to write method to shift and add least significant bit to one row above
